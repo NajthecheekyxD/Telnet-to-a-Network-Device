7d339b6e783c18d4a38028c82b1c22aa894a95d8
NajthecheekyxD <146105592+NajthecheekyxD@users.noreply.github.com>
2023-10-21 17:20:57 +0100

    telnet-to-a-network-device.py

--- telnet-to-a-network-device.py
+++ telnet-to-a-network-device.py
@@ -9,27 +9,31 @@ new_hostname = 'NewHostname'
 # Establish Telnet Connection
 telnet_connection = telnetlib.Telnet(ip_address)
 print('11')
+
 # Login to the device
 telnet_connection.read_until(b'Username: ')
 telnet_connection.write(username.encode('ascii') + b'\n')
 telnet_connection.read_until(b'Password: ')
 telnet_connection.write(password.encode('ascii') + b'\n')
 print('17')
+
 # Enter Privileged EXEC Mode
 telnet_connection.read_until(b'#')
 print('23')
-# Configure the device
-telnet_connection.write(b'configure terminal\n')
-telnet_connection.read_until(b'#')
-telnet_connection.write(f'hostname {new_hostname}\n'.encode('ascii'))
-telnet_connection.read_until(b'#')
 
-# Save the configuration
-telnet_connection.write(b'end\n')
-telnet_connection.read_until(b'#')
-telnet_connection.write(b'write memory\n')
-telnet_connection.read_until(b'#')
+# Send a command to retrieve the running configuration
+telnet_connection.write(b'show running-config\n')
+
+# Capture the running configuration output
+running_config_output = telnet_connection.read_until(b'#')
+
+# Save the running configuration to a local file
+with open('running_config.txt', 'w') as config_file:
+    config_file.write(running_config_output.decode('ascii'))
+
 print("35")
+
 # Close the Telnet Connection
 telnet_connection.write(b'quit\n')
 print("38")
+
