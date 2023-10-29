6d0e2b6de6c2f4dd29a35cedcf7995373b112096
NajthecheekyxD <146105592+NajthecheekyxD@users.noreply.github.com>
2023-10-29 13:28:41 +0000

    Telnet-to-a-network-device (2).py

--- Telnet-to-a-network-device (2).py
+++ Telnet-to-a-network-device (2).py
@@ -6,70 +6,76 @@ username = 'cisco'
 password = 'cisco123!'
 new_hostname = 'NewHostname'
 
-# Establish Telnet Connection
-telnet_connection = telnetlib.Telnet(ip_address)
-
-def main_menu():
-    while True:
-        print("Telnet Menu:")
-        print("1. Login to the device")
-        print("2. Change device hostname")
-        print("3. Save configuration")
-        print("4. Show running configuration")
-        print("5. Quit")
-
-        choice = input("Enter your choice (1/2/3/4/5): ")
-
-        if choice == '1':
-            login()
-        elif choice == '2':
-            change_hostname()
-        elif choice == '3':
-            save_configuration()
-        elif choice == '4':
-            show_running_config()
-        elif choice == '5':
-            quit_telnet()
-            break
-        else:
-            print("Invalid choice. Please select a valid option.")
-
-def login():
-    # Code to log in to the device
+def connect_to_device():
+    global telnet_connection
+    telnet_connection = telnetlib.Telnet(ip_address)
     telnet_connection.read_until(b'Username: ')
     telnet_connection.write(username.encode('ascii') + b'\n')
     telnet_connection.read_until(b'Password: ')
-    telnet_connection.write(password.encode('ascii') + b'\n')
+    telnet_connection.write(password.encode('ascii') + b'\n') 
     telnet_connection.read_until(b'#')
-    print('17') # Indicate successful login
 
-def change_hostname():
-    # Code to change the device's hostname
+def enter_privileged_exec_mode():
+    telnet_connection.read_until(b'#')
+
+def configure_device():
     telnet_connection.write(b'configure terminal\n')
     telnet_connection.read_until(b'#')
     telnet_connection.write(f'hostname {new_hostname}\n'.encode('ascii'))
     telnet_connection.read_until(b'#')
-    print('23') # Indicate hostname change
 
 def save_configuration():
-    # Code to save the configuration
     telnet_connection.write(b'end\n')
     telnet_connection.read_until(b'#')
     telnet_connection.write(b'write memory\n')
     telnet_connection.read_until(b'#')
-    print("35") # Indicate successful configuration saving
 
-def show_running_config():
-    # Code to display the running configuration
+def execute_command():
     telnet_connection.write(b'show running-config\n')
     output = telnet_connection.read_until(b'end').decode('ascii')
-    print("38") # Indicate successful running configuration output
     return output
 
-def quit_telnet():
-    # Code to close the Telnet connection
-    telnet_connection.write(b'quit\n')
-    print("42") # Indicate the closure of the Telnet connection
+def main():
+    print('=' * 50)
+    print(" Telnet Menu")
+    print('=' * 50)
+    print("1. Connect to device")
+    print("2. Enter Privileged EXEC Mode")
+    print("3. Configure the device")
+    print("4. Save the configuration")
+    print("5. Execute a command")
+    print("6. Quit")
+    print('=' * 50)
+
+    while True:
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+            continue
+
+        if choice == 1:
+            connect_to_device()
+            print("Successfully connected to the device.")
+        elif choice == 2:
+            enter_privileged_exec_mode()
+            print("Successfully entered Privileged EXEC Mode.")
+        elif choice == 3:
+            configure_device()
+            print("Successfully configured the device.")
+        elif choice == 4:
+            save_configuration()
+            print("Successfully saved the configuration.")
+        elif choice == 5:
+            output = execute_command()
+            with open('running_config.txt', 'w') as file:
+                file.write(output)
+            print("Running configuration saved to: running_config.txt")
+        elif choice == 6:
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please choose an option from the menu.")
 
 if __name__ == "__main__":
-    main_menu()
+    main()
