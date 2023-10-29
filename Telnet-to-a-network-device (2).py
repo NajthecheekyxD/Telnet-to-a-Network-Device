a2bafa1aca7ed04bb8899edcf2983d0c59580db4
NajthecheekyxD <146105592+NajthecheekyxD@users.noreply.github.com>
2023-10-29 15:35:54 +0000

    Add files via upload

--- Telnet-to-a-network-device (2).py
+++ Telnet-to-a-network-device (2).py
@@ -6,71 +6,68 @@ username = 'cisco'
 password = 'cisco123!'
 new_hostname = 'NewHostname'
 
-def login_to_device():
-    global telnet_connection
-    telnet_connection = telnetlib.Telnet(ip_address) #Establish Telnet Connection
-    telnet_connection.read_until(b'Username: ')
-    telnet_connection.write(username.encode('ascii') + b'\n')
-    telnet_connection.read_until(b'Password: ')
-    telnet_connection.write(password.encode('ascii') + b'\n') 
-    telnet_connection.read_until(b'#') # Entering into Privileged EXEC mode
+#Establish Telnet Connection
+telnet_connection = telnetlib.Telnet(ip_address)
 
+#Login to the device
+telnet_connection.read_until(b'Username: ')
+telnet_connection.write(username.encode('ascii') + b'\n')
+telnet_connection.read_until(b'Password:')
+telnet_connection.write(password.encode('ascii') + b'\n')
 
-def configure_device():
-    telnet_connection.write(b'configure terminal\n')
-    telnet_connection.read_until(b'#')
-    telnet_connection.write(f'hostname {new_hostname}\n'.encode('ascii'))
-    telnet_connection.read_until(b'#')
+#Enter Privileged EXEC Mode
+telnet_connection.read_until(b'#')
 
-def save_configuration():
-    telnet_connection.write(b'end\n')
-    telnet_connection.read_until(b'#')
-    telnet_connection.write(b'write memory\n')
-    telnet_connection.read_until(b'#')
+def display_menu():
+    print("\n-" *20)
+    print(" Menu")
+    print("-" * 20)
+    print("1. Display Running Configuration")
+    print("2. Change Hostname")
+    print("3. Exit")
+    print("-" *20)
 
-def execute_command():
-    telnet_connection.write(b'show running-config\n')
-    output = telnet_connection.read_until(b'end').decode('ascii')
-    return output
+while True:
+    display_menu()
+    choice = input ("Enter your choice: ")
 
-def main():
-    print('=' * 50)
-    print(" Telnet Menu")
-    print('=' * 50)
-    print("1. Login")
-    print("2. Configure the device")
-    print("3. Save the configuration")
-    print("4. Execute a command")
-    print("5. Quit")
-    print('=' * 50)
+    if choice == "1":
+        # Send a command to the remote device to output the running configuration
+        telnet_connection.write(b'show running-config\n')
+        output = telnet_connection.read_until(b'end').decode('ascii')
 
-    while True:
-        try:
-            choice = int(input("Enter your choice: "))
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-            continue
-
-        if choice == 1:
-            login_to_device()
-            print("Successfully Logged into the device.")
+        #Save the output to a file
+        with open('running_config.txt', 'w') as file:
+            file.write(output)
         
-        elif choice == 2:
-            configure_device()
-            print("Successfully configured the device.")
-        elif choice == 3:
-            save_configuration()
-            print("Successfully saved the configuration.")
-        elif choice == 4:
-            output = execute_command()
-            with open('running_config.txt', 'w') as file:
-                file.write(output)
-            print("Running configuration saved to: running_config.txt")
-        elif choice == 5:
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice. Please choose an option from the menu.")
+        print(f"\n Running configuration saved to: running_config.txt")
 
-if __name__ == "__main__":
-    main()
+    elif choice == "2":
+        new_hostname = input("Enter the new hostname: ")
+
+        #Configure the device
+        telnet_connection.write(b'configure terminal\n')
+        telnet_connection.read_until(b'#')
+        telnet_connection.write(f'hostname {new_hostname}\n'.encode('ascii'))
+        telnet_connection.read_until(b'#')
+
+        #Save the configuration
+        telnet_connection.write(b'end\n')
+        telnet_connection.read_until(b'#')
+        telnet_connection.write(b'write memory\n')
+        telnet_connection.read_until(b'#')
+
+        print(f"\n Hostname changed to: {new_hostname}")
+    elif choice == "3":
+        break
+    else:
+        print("\n Invalid Choice! Please Try Again.")
+
+# Close the Telnet Connection
+telnet_connection.write(b'quit\n')
+
+print("\n-"*50)
+print(f" Success!\n Device IP: {ip_address}\n Username: {username}\n Password: {password}")
+print("-"*50)
+
+
