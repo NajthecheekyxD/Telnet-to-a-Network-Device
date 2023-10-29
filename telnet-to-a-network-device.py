d0abe7dfc9b87f6c231cfc2ac108650e377e6b91
NajthecheekyxD <146105592+NajthecheekyxD@users.noreply.github.com>
2023-10-29 13:12:08 +0000

    Add files via upload

--- telnet-to-a-network-device.py
+++ telnet-to-a-network-device.py
@@ -8,18 +8,18 @@ new_hostname = 'NewHostname'
 
 # Establish Telnet Connection
 telnet_connection = telnetlib.Telnet(ip_address)
-print('11')  # Prints lines from 11-42
+
 
 # Login to the device
 telnet_connection.read_until(b'Username: ')
 telnet_connection.write(username.encode('ascii') + b'\n')
 telnet_connection.read_until(b'Password: ')
 telnet_connection.write(password.encode('ascii') + b'\n')
-print('17')  
+ 
 
 # Enter Privileged EXEC Mode
 telnet_connection.read_until(b'#')
-print('23')  
+ 
 
 # Configure the device
 telnet_connection.write(b'configure terminal\n')
@@ -32,12 +32,12 @@ telnet_connection.write(b'end\n')
 telnet_connection.read_until(b'#')
 telnet_connection.write(b'write memory\n')
 telnet_connection.read_until(b'#')
-print("35")  
+ 
 
 # Send a command to the remote device to output the running configuration 
 telnet_connection.write(b'show running-config\n')
 output = telnet_connection.read_until(b'end').decode('ascii')
-print("38") 
+ 
 
 # Save the output to a file
 with open('running_config.txt', 'w') as file:
@@ -45,6 +45,9 @@ with open('running_config.txt', 'w') as file:
 
 # Close the Telnet Connection
 telnet_connection.write(b'quit\n')
-print("42")  
-
+  
+print('-' *50)
+print(f" Success!\n Device IP: {ip_address}\n Username: {username}\n Password: {password}")
+print(f" Running configuration saved to: running_config.txt\n Hostname changed to: {new_hostname}")
+print('-' *50)
 
